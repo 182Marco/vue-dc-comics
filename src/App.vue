d9029520c9e205858830b8f1f280185f84d06a67
182Marco <marcomilza@gmail.com>
2021-05-04 17:22:22 +0200

    fatto primo component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,94 @@
 <template>
   <div id="app">
+    <div class="stripe1"></div>
+    <div class="stripe2">
+      <div class="cont">
+        <!-- logo -->
+        <img class="logo" src="@/assets/img/dc-logo.png" alt="logo" />
+        <NavigateBar></NavigateBar>
+      </div>
+    </div>
+    <div class="stripe3">
+      <div class="cont">
+        <h2>-->content goese here</h2>
+      </div>
+    </div>
+    <div class="stripe4"></div>
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-  import HelloWorld from './components/HelloWorld.vue';
+  import NavigateBar from './components/NavigateBar.vue';
 
   export default {
     name: 'App',
     components: {
-      HelloWorld,
+      NavigateBar,
     },
   };
 </script>
 
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap');
+
+  :root {
+    --brand: #1e82f6;
+    --white: #fff;
+    --black: #000;
+  }
+
+  * {
+    font-family: 'Roboto', sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  }
+
+  .stripe1,
+  .stripe3 {
+    background-color: #000;
+  }
+  .stripe1 {
+    height: 9vh;
+  }
+  .stripe2,
+  .stripe3 {
+    height: 12vh;
+  }
+
+  .stripe2 > .cont {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  img.logo {
+    width: 75px;
+  }
+
+  .stripe3 > .cont {
+    display: flex;
+    align-items: center;
+    color: var(--white);
+  }
+
+  .stripe4 {
+    background-color: var(--brand);
+    height: 16vh;
+  }
+
+  .cont {
+    height: 100%;
+    width: 80%;
+    margin: 0 auto;
   }
 </style>
---
+++ src/components/NavigateBar.vue
@@ -0,0 +1,94 @@
+<template>
+  <nav>
+    <ul>
+      <li v-for="(item, i) in links" :key="i">
+        <a :href="item.link"> {{ item.nome.toUpperCase() }}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'NavigateBar',
+    data() {
+      return {
+        links: [
+          {
+            link: 'http://google.com',
+            nome: 'CHARACTERS',
+          },
+          {
+            link: 'http://google.com',
+            nome: 'COMICS',
+          },
+          {
+            link: 'http://google.com',
+            nome: 'MOVIES',
+          },
+          {
+            link: 'http://google.com',
+            nome: 'TV',
+          },
+          {
+            link: 'http://google.com',
+            nome: 'GAMES',
+          },
+          {
+            link: 'http://google.com',
+            nome: 'collectibles',
+          },
+          {
+            link: 'http://google.com',
+            nome: 'videos',
+          },
+          {
+            link: 'http://google.com',
+            nome: 'fans',
+          },
+          {
+            link: 'http://google.com',
+            nome: 'news',
+          },
+          {
+            link: 'http://google.com',
+            nome: 'shop',
+          },
+        ],
+      };
+    },
+  };
+</script>
+
+<style scoped>
+  ul,
+  nav,
+  li,
+  a {
+    height: 100%;
+  }
+  a {
+    height: 98%;
+  }
+  li {
+    display: inline-block;
+    padding: 0 0.7vw;
+    font-size: 0.7rem;
+    font-weight: 700;
+  }
+  li:last-child {
+    padding-right: 0;
+  }
+
+  a {
+    color: var(--black);
+    display: flex;
+    align-items: center;
+    border-bottom: 2px solid transparent;
+    transition: all 0.2s;
+  }
+  a:hover {
+    color: var(--brand);
+    border-bottom: 5px solid var(--brand);
+  }
+</style>
